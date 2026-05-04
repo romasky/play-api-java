7b0cf12f7a640184c73be4fe3d1d671eb437983a
romasky <skynsk@gmail.com>
2026-05-04 19:36:05 +0700

    Remove unused Constants class

--- src/test/java/com/testFramework/play_qa_api/ApiPaths.java
+++ src/test/java/com/testFramework/play_qa_api/ApiPaths.java
@@ -6,7 +6,6 @@ public class ApiPaths {
 
     public static final String HEALTH         = BASE + "/health";
     public static final String LOGIN          = BASE + "/login";
-    public static final String VERIFY_RECAPTCHA = BASE + "/verify-recaptcha";
 
     public static final String USERS_CREATE   = BASE + "/users/create";
     public static final String USERS_LIST     = BASE + "/users/list";
